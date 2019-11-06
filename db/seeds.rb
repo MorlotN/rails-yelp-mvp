11ddf07bf82383d06e1f3a408cb06370a62ce82a
Nicolas Morlot <nicolasmorlot@gmail.com>
2019-11-06 15:45:47 +0100

    finish

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,52 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Dishoom',
+    address:      '7 Boundary St, London E2 7JE',
+    phone_number:  '0628791645',
+    category:        "chinese"
+    # content: 'youpi',
+    # rating: 4
+  },
+  {
+    name:         'kfc',
+    address:      '7 Boundary St, London E2 7JE',
+    phone_number:  '0628791645',
+    category:        "french"
+    # content: 'youpi',
+    # rating: 2
+  },
+  {
+    name:         'Mcdo',
+    address:      '7 Boundary St, London E2 7JE',
+    phone_number:  '0628791645',
+    category:        "italian"
+    # content: 'youpi',
+    # rating: 3
+  },
+  {
+    name:         'Dishoom',
+    address:      '7 Boundary St, London E2 7JE',
+    phone_number:  '0628791645',
+    category:        "chinese"
+    # content: 'youpi',
+    # rating: 5
+  },
+  {
+    name:         'Dishoom',
+    address:      '7 Boundary St, London E2 7JE',
+    phone_number:  '0628791645',
+    category:        "chinese"
+    # content: 'youpi',
+    # rating: 5
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
